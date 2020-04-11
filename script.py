59fde61d50c740d4212b8a43e3c62f29fa2c3257
Ersin Gün <ersngun@gmail.com>
2020-04-11 19:06:26 +0300

    First Commit - Ersin Gün

--- script.py
+++ script.py
@@ -1,11 +1,10 @@
 import tensorflow as tf
-
 mark1 = tf.constant("Detectrone Mark I")
 session = tf.Session()
 print(session.run(mark1))
-
 print("Merhaba Dünya")
 print("Merhaba Oğuz")
 print("Merhaba Detectrone")
 print("Again Merhaba Detectrone Family")
+print(tf.__version__)
 
